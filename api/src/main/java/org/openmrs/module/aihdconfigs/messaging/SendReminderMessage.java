1c3c9ebc960e1c09943cd09538784eab655ba136
cromwel <cromwellindy@gmail.com>
2019-07-12 09:57:45 +0300

    added location to sms

--- api/src/main/java/org/openmrs/module/aihdconfigs/messaging/SendReminderMessage.java
+++ api/src/main/java/org/openmrs/module/aihdconfigs/messaging/SendReminderMessage.java
@@ -6,17 +6,26 @@ import com.africastalking.SmsService;
 import com.africastalking.sms.Recipient;
 import org.apache.commons.lang3.StringUtils;
 import org.openmrs.Patient;
+import org.openmrs.Location;
+import org.openmrs.LocationAttribute;
+import org.openmrs.LocationAttributeType;
+import org.openmrs.PatientIdentifier;
+import org.openmrs.PatientIdentifierType;
 import org.openmrs.PersonAttribute;
 import org.openmrs.PersonAttributeType;
 import org.openmrs.api.context.Context;
+import org.openmrs.api.AdministrationService;
+import org.openmrs.api.PatientService;
 import org.openmrs.calculation.patient.PatientCalculationContext;
 import org.openmrs.calculation.patient.PatientCalculationService;
 import org.openmrs.calculation.result.CalculationResultMap;
 import org.openmrs.module.aihdconfigs.ConfigCoreUtils;
 import org.openmrs.module.aihdconfigs.Dictionary;
+import org.openmrs.module.aihdconfigs.Facilities;
 import org.openmrs.module.aihdconfigs.calculation.ConfigCalculations;
 import org.openmrs.module.aihdconfigs.calculation.ConfigEmrCalculationUtils;
 import org.openmrs.module.aihdconfigs.metadata.PersonAttributeTypes;
+import org.openmrs.module.aihdconfigs.metadata.PatientIdentifierTypes;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
 import org.openmrs.util.OpenmrsUtil;
 import org.slf4j.Logger;
@@ -30,6 +39,7 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 
 public class SendReminderMessage {
@@ -73,11 +83,11 @@ public class SendReminderMessage {
 
     }
 
-    private static String missedAppointMentMesssage(String lang, String name) {
+    private static String missedAppointMentMesssage(String lang, String name, PersonAttribute facilityName) {
         if (lang.equals("english")) {
-            return String.format("Dear %s , our records show that you have missed your last appointment(s). This could affect your progress and health goals. You are advised to please urgently visit your doctor within the next 24 hours. Please visit your health care provider urgently.", name);
+            return String.format("Dear %s, our records show that you have missed your last appointment(s) at %s. This could affect your progress and health goals. You are advised to please visit your doctor or health care provider urgently.", name, facilityName);
         } else {
-            return String.format("Hujambo %s, records zetu zinatuonyesha kwamba ulikosa kutembelea kituo cha afya kama ilivyopangwa. Hio inaweza kuduru afya na malengo yako ya kiafya. Tafadhali tembelea mhudumu wako wa afya mara moja.", name);
+            return String.format("Hujambo %s, rekodi zetu zinatuonyesha kwamba ulikosa kutembelea kituo cha afya %s kama ilivyopangwa. Hii inaweza kudhuru maendeleo na malengo yako ya kiafya. Tafadhali tembelea mhudumu wako wa afya mara moja.", name, facilityName);
         }
     }
 
@@ -91,17 +101,21 @@ public class SendReminderMessage {
         Set<Patient> results = missedAppointments(context);
         PersonAttributeType attributeType = MetadataUtils.existing(PersonAttributeType.class, PersonAttributeTypes.TELEPHONE_NUMBER.uuid());
         PersonAttributeType langAttributeType = MetadataUtils.existing(PersonAttributeType.class, PersonAttributeTypes.USER_LANGUAGE.uuid());
+        PersonAttributeType patientLocation = MetadataUtils.existing(PersonAttributeType.class, PersonAttributeTypes.PATIENT_LOCATION.uuid());
 
         for (Patient patient : results) {
             String name = String.format("%s", patient.getGivenName());
             PersonAttribute personAttribute = patient.getPerson().getAttribute(attributeType);
+            PersonAttribute facilityName = patient.getPerson().getAttribute(patientLocation);
+    
             if (langAttributeType != null) {
                 PersonAttribute language = patient.getPerson().getAttribute(langAttributeType);
-                message = missedAppointMentMesssage((language != null && StringUtils.isNotEmpty(language.getValue()) ? language.getValue() : "kiswahili"), name);
+                
+                message = missedAppointMentMesssage((language != null && StringUtils.isNotEmpty(language.getValue()) ? language.getValue() : "kiswahili"), name, facilityName);
             } else {
-                message = missedAppointMentMesssage("kiswahili", name);
+                message = missedAppointMentMesssage("kiswahili", name, facilityName);
             }
-            if (StringUtils.isNotEmpty(convertPhoneNumber(personAttribute.getValue()))) {
+            if (StringUtils.isNotEmpty(convertPhoneNumber(personAttribute.getValue()))){
                 buildMessage(convertPhoneNumber(personAttribute.getValue()), message);
             }
         }
@@ -137,4 +151,52 @@ public class SendReminderMessage {
             return "";
         }
     }
+
+    // private static String getFacilityName(String name){
+    //     PersonAttributeType attributeType = MetadataUtils.existing(PersonAttributeType.class, PersonAttributeTypes.PATIENT_LOCATION.uuid());
+    //     LocationAttributeType locationAttributeType = Context.getLocationService().getLocationAttributeTypeByName("MflCode");
+    //     PersonAttributeType personAttributeTypeMflForPatient = Context.getPersonService().getPersonAttributeTypeByUuid(PersonAttributeTypes.PATIENT_MFL_CODE.uuid());
+    //     PatientService patientService = Context.getPatientService();
+    //     AdministrationService as = Context.getAdministrationService();
+    //     PatientIdentifierType pit_aihd = patientService.getPatientIdentifierTypeByUuid(PatientIdentifierTypes.AIHD_PATIENT_NUMBER.uuid());
+        
+    //     Patient p = patientService.getPatient((Integer) row.get(0));
+    //     PersonAttribute mflCodeForPatient = p.getAttribute(personAttributeTypeMflForPatient);
+    //     PersonAttribute personAttribute = p.getAttribute(attributeType);
+       
+    //         if(personAttribute != null && StringUtils.isNotEmpty(personAttribute.getValue())){
+    //             Location location = Context.getLocationService().getLocation(personAttribute.getValue());
+    //               if(location != null) {
+    //                   Set<LocationAttribute> attribute = new HashSet<LocationAttribute>(location.getAttributes());
+    //                   if(attribute.size() > 0){
+    //                       for(LocationAttribute locationAttribute: attribute){
+    //                           if(locationAttribute.getAttributeType().equals(locationAttributeType)){
+    //                               UUID uuid = UUID.randomUUID();
+    //                               PatientIdentifier aihdId = new PatientIdentifier();
+    //                               aihdId.getLocation();
+    //                           }
+    //                       }
+    //                   }
+    //               }
+    //         }
+
+    //         else if (mflCodeForPatient != null && StringUtils.isNotEmpty(mflCodeForPatient.getValue())) {
+    //             List<List<Object>> location = as.executeSQL("SELECT location_id FROM location_attribute WHERE value_reference='"+mflCodeForPatient.getValue()+"'", true);
+    //             if(location.size() > 0) {
+    //                 for (List<Object> loc : location) {
+    //                     Location useLocation = Context.getLocationService().getLocation((Integer) loc.get(0));
+    //                     if(useLocation != null) {
+    //                         UUID uuid = UUID.randomUUID();
+    //                         PatientIdentifier aihdId = new PatientIdentifier();
+    //                         aihdId.setLocation(useLocation);
+    //                         aihdId.getLocation();
+    //                     }
+    //                 }
+    //             }
+
+    //             }
+            
+     
+    //     return name;
+    // }
 }
